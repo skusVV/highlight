bba293045335ad4ecaf4c410a69b5f527fa085e3
vskus <vskus@beyond.ai>
2024-01-05 17:31:45 +0200

    added modifications to work with all websites

---
+++ src/pages/Content/helpers.js
@@ -0,0 +1,48 @@
+export function removeHighlights() {
+    const highlightedElements = document.querySelectorAll('.highlight-element-ex-243');
+
+    highlightedElements.forEach(element => {
+        const textNode = document.createTextNode(element.textContent);
+
+        element.parentNode.replaceChild(textNode, element);
+    });
+}
+
+export function isAlreadyHighlighted(node) {
+    return node.parentNode.className === 'highlight-element-ex-243';
+}
+
+export function removeRedundant(keywords) {
+    const highlightedElements = document.querySelectorAll('.highlight-element-ex-243');
+    const keywordsValues = keywords.map(item => item.value)
+    const itemsToRemove = new Array(...highlightedElements).filter(item => !keywordsValues.includes(item.innerText.toLowerCase()));
+    itemsToRemove.forEach(item => {
+        const textNode = document.createTextNode(item.textContent);
+
+        item.parentNode.replaceChild(textNode, item);
+    });
+}
+
+export function highlightWords(node, keywords) {
+    if (node.nodeType === 3 && !isAlreadyHighlighted(node) && node.tagName !== 'STYLE') { // Text node
+        let text = node.nodeValue;
+        let replacedText = text;
+        let color = 'yellow';
+
+        keywords.forEach(keyword => {
+            const regex = new RegExp(keyword.value, 'gi');
+            color = keyword.color;
+            replacedText = replacedText.replace(regex, (match) => {
+                return `<span class="highlight-element-ex-243" style="background-color: ${color}">${match}</span>`;
+            });
+        });
+
+        if (replacedText !== text) {
+            let newNode = document.createElement('span');
+            newNode.innerHTML = replacedText;
+            node.parentNode.replaceChild(newNode, node);
+        }
+    } else if (node.nodeType === 1 && node.tagName !== 'STYLE') { // Element node
+        node.childNodes.forEach(child => highlightWords(child, keywords));
+    }
+}
--- src/pages/Content/index.js
+++ src/pages/Content/index.js
@@ -1,93 +1,70 @@
+// TODO Upwork <span>Report</span>s issue.
+import { removeHighlights, removeRedundant, highlightWords } from './helpers';
 
-
+// most likely need to use set with key of tabId
+// const data = {
+//     'tab-1': {
+//         keywords: [],
+//         isExtEnabled: true
+//     },
+// }
 let keywords = [];
 let isExtEnabled = true;
 
 const style = document.createElement('style');
-style.textContent = `.highlight-element-ex-243 {  color: black;}`;
+style.textContent = `.highlight-element-ex-243 { color: black;}`;
 document.head.appendChild(style);
 
-function isAlreadyHighlighted(node) {
-    return node.parentNode.className === 'highlight-element-ex-243';
-}
-
-function highlightWords(node) {
-    if (node.nodeType === 3 && !isAlreadyHighlighted(node)) { // Text node
-        let text = node.nodeValue;
-        let replacedText = text;
-        let color = 'yellow';
-
-        keywords.forEach(keyword => {
-            const regex = new RegExp(keyword.value, 'gi');
-            color = keyword.color;
-            replacedText = replacedText.replace(regex, (match) => {
-                return `<span class="highlight-element-ex-243" style="background-color: ${color}">${match}</span>`;
-            });
-        });
-
-        if (replacedText !== text) {
-            let newNode = document.createElement('span');
-            newNode.innerHTML = replacedText;
-            node.parentNode.replaceChild(newNode, node);
-        }
-    } else if (node.nodeType === 1) { // Element node
-        node.childNodes.forEach(child => highlightWords(child));
-    }
-}
-function removeHighlights() {
-    const highlightedElements = document.querySelectorAll('.highlight-element-ex-243');
-
-    highlightedElements.forEach(element => {
-        const textNode = document.createTextNode(element.textContent);
-
-        element.parentNode.replaceChild(textNode, element);
-    });
-}
-
-function removeRedundant() {
-    const highlightedElements = document.querySelectorAll('.highlight-element-ex-243');
-    const keywordsValues = keywords.map(item => item.value)
-    const itemsToRemove = new Array(...highlightedElements).filter(item => !keywordsValues.includes(item.innerText.toLowerCase()));
-    itemsToRemove.forEach(item => {
-        const textNode = document.createTextNode(item.textContent);
-
-        item.parentNode.replaceChild(textNode, item);
-    });
-}
 
 const runHighlight = () => {
     if (isExtEnabled) {
-        highlightWords(document.body);
-        removeRedundant();
+        highlightWords(document.body, keywords);
+        removeRedundant(keywords);
     } else {
         removeHighlights();
     }
 }
 
+const syncDataWithPopUp = () => {
+    chrome?.storage?.local?.get(['key'], function(result) {
+        try {
+            keywords = JSON.parse(result.key)
+        } catch (e) {}
+    });
+
+    chrome?.storage?.local?.get(['isEnabled'], function(result) {
+        try {
+            isExtEnabled = result.isEnabled === 'true';
+            runHighlight()
+        } catch (e) {}
+    });
+}
+
+let storageInterval;
+let runHighlightInterval;
 
 const init = () => {
-    if(!window?.location?.origin?.toLowerCase().includes('linkedin')) {
-        return;
-    }
-
-    setInterval(() => {
-        chrome?.storage?.local?.get(['key'], function(result) {
-            try {
-                keywords = JSON.parse(result.key)
-            } catch (e) {}
-        });
-
-        chrome?.storage?.local?.get(['isEnabled'], function(result) {
-            try {
-                isExtEnabled = result.isEnabled === 'true';
-                runHighlight()
-            } catch (e) {}
-        });
+    storageInterval = setInterval(() => {
+        syncDataWithPopUp();
     }, 1000);
 
-    setInterval(() => {
+    runHighlightInterval = setInterval(() => {
         runHighlight();
     }, 3000);
 }
-
 init();
+
+// Switch ON/OFF when leave the website.
+function handleVisibilityChange() {
+    if (document.visibilityState === "visible") {
+        setTimeout(() => {
+            syncDataWithPopUp()
+            runHighlight();
+        }, 500);
+        init();
+    } else {
+        clearInterval(storageInterval);
+        clearInterval(runHighlightInterval);
+    }
+}
+document.addEventListener("visibilitychange", handleVisibilityChange);
